11a6342778cdc24aa46100bf8443272ff586ba1d
Tassany <tassanyonofre@gmail.com>
2022-05-31 02:18:43 -0300

    Agora vai

--- src/agoraVai.py
+++ src/agoraVai.py
@@ -1,35 +1,51 @@
-
-
-
-def ReadTxt():
-    arg = open("dados.txt","r")
-    msg = []
-    for linha in arg:
-        msg.append(linha)
-    print(msg)
-    arg.close()
-    return msg
+import string
+from symtable import Symbol
+import os
 
 
 def DecompressLzw(dic,code):
     for i in range(len(dic)):
-
         pass
 
 
-def CompressLzw(dic,msg):
+def CompressLzw():
+    print("Comprimindo arquivo!")
+    file = open("dados.txt",encoding="latin-1")
+    msg = file.read()
+    maximum_size = pow(2,14) 
+    dic_size = 256
+    dic = []
+    for i in range(dic_size):          
+        dic.append(i)
+    print(dic)
+    code = []
+    msg_plus = ""
 
+    for i in range(len(msg)-1):
+        auxMsg = msg[i]
+        oneMsg = msg[i+1]
+        msg_plus = auxMsg + oneMsg
 
+        try:
+            index_value = dic.index(msg_plus)
+        except ValueError:
+            index_value = -1
 
+        if index_value != 1:
+            auxMsg += oneMsg
+        else:
+            code.append(i)
+        dic.append(auxMsg)
+        auxMsg = oneMsg
 
-
+    print("Codigo: ")
+    print(code)
 
 
 def main():
-    # msg = ReadTxt()
-    dic = ["a","b"]
-    msg = "aabababaaa"
-    CompressLzw(dic, msg)
+
+    CompressLzw()
+    
     
 
 if __name__ == "__main__":
